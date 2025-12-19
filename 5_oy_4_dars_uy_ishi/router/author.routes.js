654f57aed2cf37c7ced0df8e5e73ac24c45722b0
Qudrat377 <assomad377@gmail.com>
2025-12-19 23:58:03 +0500

    5_oy_4_dars_uy_ishi

---
+++ 5_oy_4_dars_uy_ishi/router/author.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express")
+const { getAllAuthors, addAuthor, getOneAutor, updateAuthor, deleteAuhtor } = require("../controller/author.controller")
+
+const authorRouter = Router()
+
+authorRouter.get("/get_all_authors", getAllAuthors)
+authorRouter.post("/add_author", addAuthor)
+authorRouter.get("/get_one_author/:id", getOneAutor)
+authorRouter.put("/update_author/:id", updateAuthor)
+authorRouter.delete("/delete_author/:id", deleteAuhtor)
+
+module.exports = authorRouter
---
+++ 5_oy_4_dars_uy_ishi/schema/book.schema.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+
+const Book = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    pages: {
+      type: Number,
+      required: true,
+    },
+    published_year: {
+      type: String,
+      required: true,
+    },
+    image_url: {
+      type: String,
+      required: true,
+    },
+    discription: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    period: {
+      type: String,
+      required: true,
+    },
+    published_home: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const BookSchema = model("Book", Book);
+
+module.exports = BookSchema;
